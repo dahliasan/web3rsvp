9df56a3035d0e58c36f60354446e23c7e7b6c692
Dahlia Foo <dahliafoo@gmail.com>
2022-09-15 09:24:07 +1000

    deploy to mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,5 +18,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+    solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            chainId: 31337, // 1337
+        },
+        mumbai: {
+            url: process.env.STAGING_INFURA_URL,
+            accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+            gas: 2100000,
+            gasPrice: 8000000000,
+        },
+    },
+}
